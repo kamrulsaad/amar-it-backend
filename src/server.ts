c14e5eec8540ac9d40f3a2c42b1aa4e95039a198
Kamrul Saad <skamrul2680@gmail.com>
2023-10-12 01:47:23 +0600

    Added Husky, Lint-staged and loger for better server setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
-import app from "./app";
-import config from "./config";
+import app from './app';
+import config from './config';
+import { errorLogger, logger } from './shared/logger';
+import { Server } from 'http';
 
-async function main() {
-  console.log("Connected to MongoDB");
-  app.listen(config.port, () => {
-    console.log(`Server running at port ${config.port}`);
+process.on('uncaughtException', error => {
+  errorLogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
+
+async function bootstrap() {
+  try {
+    logger.info('✅ Database connected successfully');
+    app.listen(config.port, () => {
+      logger.info(`🚀 Server running on port ${config.port}`);
+    });
+  } catch (error) {
+    errorLogger.error(`❌ Error while connecting to database: ${error}`);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(`Error: ${error} ❌`);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
   });
 }
 
-main();
+bootstrap();
