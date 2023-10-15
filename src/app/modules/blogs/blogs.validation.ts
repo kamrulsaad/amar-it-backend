1095401a0ef2538a5553adac8a997d06edb7358a
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-10-16 03:22:48 +0600

    blogs crud operation done by me

---
+++ src/app/modules/blogs/blogs.validation.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+
+const createBlogZodSchema = z.object({
+  blogs: z.object({
+    title: z.string({
+      required_error: 'Title is required',
+    }),
+    content: z.string({
+      required_error: 'Content is required',
+    }),
+    image: z.string({
+      required_error: 'Image is required',
+    }),
+    blogCategoryId: z.string({
+      required_error: 'Blog Category is required',
+    }),
+  }),
+});
+
+const updateBlogZodSchema = z.object({
+  blogs: z.object({
+    title: z
+      .string({
+        required_error: 'Title is required',
+      })
+      .optional(),
+    content: z
+      .string({
+        required_error: 'Content is required',
+      })
+      .optional(),
+    image: z
+      .string({
+        required_error: 'Image is required',
+      })
+      .optional(),
+    blogCategoryId: z
+      .string({
+        required_error: 'Blog Category is required',
+      })
+      .optional(),
+  }),
+});
+
+export const BlogValidation = {
+  createBlogZodSchema,
+  updateBlogZodSchema,
+};
