e1f37cb39d9bc131c97ccab18d1a692c91036626
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-10-16 13:21:04 +0600

    home-banner
    createing home-banner with admin

---
+++ src/app/modules/home-banner/home-banner.service.ts
@@ -0,0 +1,94 @@
+import { HomeBannerContents } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { FileUploadHelper } from '../../../helpers/FileUploadHelper';
+import { IUploadFile } from '../../../interface/file';
+import prisma from '../../../shared/prisma';
+
+const insertIntoDB = async (
+  data: HomeBannerContents,
+  file: IUploadFile,
+): Promise<HomeBannerContents> => {
+  const uploadedImage = await FileUploadHelper.uploadToCloudinary(file);
+  if (!uploadedImage) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Image upload failed');
+  }
+  return await prisma.$transaction(async tx => {
+    return tx.homeBannerContents.create({
+      data: {
+        ...data,
+        image: uploadedImage.secure_url as string,
+      },
+    });
+  });
+};
+
+const getAllFromDB = async (): Promise<HomeBannerContents[]> => {
+  return await prisma.homeBannerContents.findMany();
+};
+
+const getByIdFromDB = async (
+  id: string,
+): Promise<HomeBannerContents | null> => {
+  return await prisma.homeBannerContents.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+const updateIntoDB = async (
+  id: string,
+  file: IUploadFile,
+  payload: Partial<HomeBannerContents>,
+): Promise<HomeBannerContents> => {
+  return await prisma.$transaction(async tx => {
+    const isExistHomeBanner = await tx.homeBannerContents.findUnique({
+      where: { id },
+    });
+
+    if (!isExistHomeBanner) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Home Banner not found');
+    }
+
+    if (file && isExistHomeBanner.image) {
+      const response = await FileUploadHelper.replaceImage(
+        isExistHomeBanner.image,
+        file,
+      );
+      if (!response) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Image upload failed');
+      }
+      payload.image = response.secure_url as string;
+    }
+
+    return tx.homeBannerContents.update({
+      where: { id },
+      data: payload,
+    });
+  });
+};
+
+const deleteFromDB = async (id: string): Promise<HomeBannerContents> => {
+  return await prisma.$transaction(async tx => {
+    const isExistHomeBanner = await tx.homeBannerContents.findUnique({
+      where: { id },
+      select: { image: true },
+    });
+
+    if (isExistHomeBanner?.image) {
+      await FileUploadHelper.destroyToCloudinary(isExistHomeBanner.image);
+    }
+    return tx.homeBannerContents.delete({
+      where: { id },
+    });
+  });
+};
+
+export const HomeBannerService = {
+  insertIntoDB,
+  getAllFromDB,
+  getByIdFromDB,
+  updateIntoDB,
+  deleteFromDB,
+};
---
+++ src/app/modules/home-banner/home-banner.validation.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+const createHomeBannerZodSchema = z.object({
+  title: z.string({
+    required_error: 'Title is required',
+  }),
+  content: z.string({
+    required_error: 'Content is required',
+  }),
+  image: z.string({
+    required_error: 'Image is required',
+  }),
+});
+
+const updateHomeBannerZodSchema = z.object({
+  title: z
+    .string({
+      required_error: 'Title is required',
+    })
+    .optional(),
+  content: z
+    .string({
+      required_error: 'Content is required',
+    })
+    .optional(),
+  image: z
+    .string({
+      required_error: 'Image is required',
+    })
+    .optional(),
+});
+
+export const HomeBannerValidation = {
+  createHomeBannerZodSchema,
+  updateHomeBannerZodSchema,
+};
