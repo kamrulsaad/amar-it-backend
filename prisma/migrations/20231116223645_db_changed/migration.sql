22beb3225932cf95295b412921e02ca60b3c94bc
Kamrul Saad <skamrul2680@gmail.com>
2023-11-17 05:40:56 +0600

    Backend Completed

---
+++ prisma/migrations/20231116223645_db_changed/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `payments` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "payments" DROP CONSTRAINT "payments_customerId_fkey";
+
+-- DropTable
+DROP TABLE "payments";
