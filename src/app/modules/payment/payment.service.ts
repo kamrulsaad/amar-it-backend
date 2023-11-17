5581e09661f370a8d1950fc298a9d793be0ad3ba
Noormohammad011 <noormohammad.net1503011@gmail.com>
2023-11-17 14:27:47 +0600

    payment done
    payment with stripe payment

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,19 +1,11 @@
 import { Payments } from '@prisma/client';
 import httpStatus from 'http-status';
 import Stripe from 'stripe';
-import { uuid } from 'uuidv4';
-// import config from '../../../config';
+import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
-// const stripe_secret_key = `${config.stripe_secret_key}` as string;
-
-const stripe = new Stripe(
-    'sk_test_51OCpX2EoZ8jVom2AZhjDKzHbLSHyBf4XFjZWAf42X8QzCmTmA8xx6cBMHQhrKDKSof5L9lotLwARbkCdYNYKlVM500ZBa5GtwT',
-    {
-        apiVersion: '2022-11-15',
-    },
-);
+const stripe = new Stripe(config.stripe_secret_key as string);
 
 const insertIntoDB = async (data: {
     //eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,7 +17,6 @@ const insertIntoDB = async (data: {
 
     const result = await prisma.$transaction(
         async tx => {
-            let paymentResult = null;
             const user = await tx.customer.findUnique({
                 where: {
                     username,
@@ -40,29 +31,24 @@ const insertIntoDB = async (data: {
                 email: stripeToken.email,
             };
             const customer = await stripe.customers.create(params);
-            const payment = await stripe.charges.create(
-                {
-                    amount: totalPrice * 100,
-                    currency: 'usd',
-                    customer: customer.id,
-                    receipt_email: stripeToken.email,
-                },
-                {
-                    stripeAccount: uuid(),
-                },
-            );
-            if (payment) {
-                paymentResult = await tx.payments.create({
-                    data: {
-                        totalPrice: payment.amount / 100,
-                        isPaid: true,
-                        paymentID: payment.id,
-                        customerId: user.id,
-                    },
-                });
+            const payment = await stripe.charges.create({
+                amount: totalPrice * 100,
+                currency: 'usd',
+                customer: customer.id,
+                description: `Payment for ${user.username} || ${customer.id} for ${totalPrice} USD `,
+            });
+            if (!payment) {
+                throw new ApiError(httpStatus.BAD_REQUEST, 'Payment failed');
             }
 
-            return paymentResult;
+            return await tx.payments.create({
+                data: {
+                    totalPrice: payment.amount / 100,
+                    isPaid: true,
+                    paymentID: payment.id,
+                    customerId: user.id,
+                },
+            });
         },
         { timeout: 10000 },
     );
